76be305565d198b491ace386a02566344e84c7e6
Dione <dionemorales@outlook.com>
2022-03-06 13:12:55 +1100

    updated release script to be ran in project root

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,4 @@
 cargo zigbuild --release --target aarch64-unknown-linux-gnu 
-cp ../target/aarch64-unknown-linux-gnu/release/commit-checker ../build/bootstrap  
+cp ./target/aarch64-unknown-linux-gnu/release/commit-checker ./build/bootstrap
 sam deploy
+rm -rf ./build/
