922dd32799ffbb7dacfb0d87c32ea2e4c274bac9
Dione <dionemorales@outlook.com>
2022-03-08 22:56:37 +1100

    added dependencies needed for sending text message and fixed release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
-cargo zigbuild --release --target aarch64-unknown-linux-gnu 
+cargo zigbuild --release --target aarch64-unknown-linux-gnu
+mkdir build
 cp ./target/aarch64-unknown-linux-gnu/release/commit-checker ./build/bootstrap
 sam deploy
-rm -rf ./build/
+rm -rf build
